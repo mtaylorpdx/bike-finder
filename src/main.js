a95f83d2cbc1fa3cc33cdb414a98e8cbfeb0b4e4
Michelle Morin and Matt Taylor <michelle@>
2020-02-12 16:08:25 -0800

    refactor logic

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import './styles.css';
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    $("form").hide();
+    $("#inputs").hide();
     $("#results").show();
     const searchRadius = parseFloat($("input#distance").val()); 
     let location = $("input#location").val();
@@ -34,16 +34,41 @@ $(document).ready(function() {
       } else if (response.bikes.length === 0) {
         $("#results").append(`<div class='row'><div class="col-md-12">There are no stolen bikes in your requested location. <a href='index.html'>Click here</a> to try another location.</div></div>`);
       } else if (response.bikes.length > 0) {
+        // Logic for most commonly stolen manufacturer:
+        let manufacturerArray = [];
+        response.bikes.forEach(function(bike){ 
+          manufacturerArray.push(bike.manufacturer_name);
+        });
+        let maximumFrequency = 1;
+        let counter = 0;
+        let manufacturer;
+        for (let i = 0; i<manufacturerArray.length; i++){
+          for (let j=i; j<manufacturerArray.length; j++){
+            if (manufacturerArray[i] === manufacturerArray[j]) {
+              counter++;
+            }
+            if (maximumFrequency < counter) {
+              maximumFrequency = counter;
+              manufacturer = manufacturerArray[i];
+            }
+          }
+          counter=0;
+        }
+        $(".snapshot").html(manufacturer);
+        $(".maxFreq").html(maximumFrequency);
+
+        // Logic for bike list:
         response.bikes.forEach(function(bike) {
           let unix_timestamp = `${bike.date_stolen}`;
           let date = new Date(unix_timestamp *1000);
           let bikeThumb;
+          let bikeID = bike.id
           if (bike.thumb != null) {
             bikeThumb = bike.thumb;
           } else {
             bikeThumb = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSWRA8lngFHUYDhz3N2Ct1y27pEw149bMN9tKqrZQNuER3vwdmh";
           }
-          $("#results").append(`<div class='row'><div class='col-md-4'><strong>${bike.manufacturer_name}</strong> ${bike.frame_model}</div><div class='col-md-4'>${date}</div><div class='col-md-4'><img src='${bikeThumb}' alt='A photo of bike' height='200px' width='200px'></div> </div>`);
+          $("#bikeStats").append(`<div class='row'><div class='col-md-4'><strong>${bike.manufacturer_name}</strong> ${bike.frame_model}</div><div class='col-md-4'>${date}</div><div class='col-md-4'><center><img src='${bikeThumb}' alt='A photo of bike' height='200px' width='200px'></center></div> </div>`);
         });
       }
     };
